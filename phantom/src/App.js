1435a51862618bceaf04196f7dbdaa075091488c
Avinash <rajaavinash1001@gmail.com>
2023-03-17 15:52:11 +0530

    useless commit

---
+++ phantom/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Provider from './walletProvider/provider';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Provider/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
